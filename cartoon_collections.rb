a59f445f5a8a8e8b3f380be208bc01e361ca1bb5
Maximilian Rex Alba <albam476@gmail.com>
2022-12-04 23:22:18 -0500

    commit tree eeba228bf5d8c21d978c45ba27b25db1cc92313c

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves arr
   # Your code here
+  arr.each.with_index(1) do |name, i|
+    puts "#{i}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet arr
   # Your code here
+  arr.map do |name|
+    "#{name.capitalize}!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls call_arr
   # Your code here
+  call_arr.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese snack_arr
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snack_arr.find do |snack|
+    puts snack
+    cheese_types.include?(snack)
+  end
+  
 end
+
